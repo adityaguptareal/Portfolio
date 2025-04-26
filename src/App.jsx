0ddc6e6ccd2f5f2821023194f037732dd77860d1
Aditya Kumar Gupta <78353347+adityaguptareal@users.noreply.github.com>
2025-04-26 22:27:19 +0530

    Added some section

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,23 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Hero from './sections/Hero'
+import ShowcaseSection from './components/ShowcaseSection'
+import Navbar from './components/Navbar'
+import LogoSection from './components/LogoSection'
+import FeaturedCard from './components/FeaturedCard'
+import ExperienceSection from './components/ExperienceSection'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
+      <Navbar/>
       <Hero/>
+      <ShowcaseSection/>
+      <LogoSection/>
+      <FeaturedCard/>
+      <ExperienceSection/>
     </>
   )
 }
