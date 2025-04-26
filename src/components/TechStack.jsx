a213927ead6708d512381ac01771c28a33f6118a
Aditya Kumar Gupta <78353347+adityaguptareal@users.noreply.github.com>
2025-04-26 23:50:38 +0530

    Added tech stack component

--- src/components/TechStack.jsx
+++ src/components/TechStack.jsx
@@ -1,19 +1,58 @@
-import React from 'react'
-import TitleHeader from './TitleHeader'
-import { techStackIcons } from '../constants'
+import React, { useEffect } from "react";
+import TitleHeader from "./TitleHeader";
+import { techStackIcons } from "../constants";
+import TechIcon from "./TechIcon";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger);
 
 function TechStack() {
-  return (
-    <div id='skills' className='flex-center section-padding'>
-    <div className='w-full h-full md:px-10 px-5'>
-        <TitleHeader title="My Preferred Tech Stack" sub="The Skills I Bring to the Table"/> 
-        <div className="tech-grid">
-            {techStackIcons.map((icon,index)=>{})}
-        </div>
-    </div>
+  useEffect(() => {
+    gsap.fromTo(
+      ".tech-card",
+      { y: 100, opacity: 0 },
+      {
+        y: 0,
+        opacity: 1,
+        duration: 1,
+        stagger: 0.2,
+        ease: "power2.inOut",
+        scrollTrigger: {
+          trigger: "#skills",
+          start: "top center",
+        },
+      }
+    );
+  }, []);
 
+  return (
+    <div id="skills" className="flex-center section-padding">
+      <div className="w-full h-full md:px-10 px-5">
+        <TitleHeader
+          title="My Preferred Tech Stack"
+          sub="The Skills I Bring to the Table"
+        />
+        <div className="tech-grid">
+          {techStackIcons.map((icon) => (
+            <div
+              key={icon.name}
+              className="card-border tech-card overflow-hidden group xl:rounded-full rounded-lg"
+            >
+              <div className="tech-card-content">
+                <div className="tech-icon-wrapper">
+                  <TechIcon model={icon} />
+                </div>
+                <div className="padding-x w-full">
+                  <p>{icon.name}</p>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default TechStack
+export default TechStack;
