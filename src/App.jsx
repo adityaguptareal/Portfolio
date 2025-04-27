9b4f0021fb656f089e1004e7324c490d233570f4
Aditya Kumar Gupta <78353347+adityaguptareal@users.noreply.github.com>
2025-04-27 10:21:35 +0530

    Deploying for production

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { useEffect } from "react";
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import Hero from "./sections/Hero";
 import ShowcaseSection from "./components/ShowcaseSection";
